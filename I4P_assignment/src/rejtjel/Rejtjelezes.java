be3efb4a3ff6b76e3c54f647989d950a91c6648d
KBeni <benedekkozel@gmail.com>
2024-06-14 02:27:27 +0200

    Létrehoztam két függvényt, egyet numberToChar, másikat rejtjelez néven, plusz csináltam egy Visszefejt nevű java file-t
    A numberToChar-t azért csináltam, hogy majd a rejtjelez függvényben miután kiszámoltam a rejtjelezett üzenet karakterkódját, utána visszatudjam alakítani character formátumba. A main method elhelyezésében még nem vagyok biztos, csak tesztelés céljából van a file-ban.

--- I4P_assignment/src/rejtjel/Rejtjelezes.java
+++ I4P_assignment/src/rejtjel/Rejtjelezes.java
@@ -11,4 +11,24 @@ public class Rejtjelezes {
 		}
 		throw new Exception("Error with this character:" + betu);
 	}
+	public static char numberToChar(int kod) {
+		return karakterek[kod];
+	}
+	public static void main(String args[]) {
+		String uzenet = "helloworld";
+		String kulcs = "abcdefgijkl";
+		String rejtjelezett = rejtjelez(uzenet, kulcs);
+		System.out.println(rejtjelezett);
+	}
+	public static String rejtjelez(String uzenet, String kulcs) {
+		String rejt_uzenet = "";
+		for (int i = 0; i < uzenet.length(); i++) {
+			try {
+				rejt_uzenet += Rejtjelezes.numberToChar((Rejtjelezes.charToNumber(uzenet.charAt(i)) + Rejtjelezes.charToNumber(kulcs.charAt(i))) % 27);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+		return rejt_uzenet;
+	}
 }
---
+++ I4P_assignment/src/rejtjel/Visszafejt.java
@@ -0,0 +1,5 @@
+package rejtjel;
+
+public class Visszafejt {
+
+}
